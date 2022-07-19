91f4cf00abe9703061f7a18dadece8656508f580
goranaviani <goranaviani@gmail.com>
2022-07-19 21:48:48 +0200

    communicating with props

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,11 +3,15 @@ import React from "react";
 const Comment = () => {
 
     return (
-        <ul>
-            <li>Coffee</li>
-            <li>Tea</li>
-            <li>Milk</li>
-        </ul>
+        <>
+            <ul>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
+            ------------------------------
+        </>
+
     )
 }
 export default Comment;
