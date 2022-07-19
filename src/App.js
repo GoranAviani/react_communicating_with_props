176e76623a3d5228377099fc4a30246680a34357
goranaviani <goranaviani@gmail.com>
2022-07-19 21:47:21 +0200

    communicating with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Comment from "./components/Comment";
 
 const App = () => {
 
     return (
         <>
             APP
+            <Comment/>
         </>)
 }
 
